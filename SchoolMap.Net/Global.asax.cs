efb2b32fdd980d583d0a3e8dcec3e57b71438c55
WestDiscGolf <git@icecubez.net>
2012-07-17 20:53:50 +0100

    RavenDb unsta ble release added to avoid json.net dependancy

--- SchoolMap.Net/Global.asax.cs
+++ SchoolMap.Net/Global.asax.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -12,10 +8,13 @@ namespace SchoolMap.Net
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            // to plumb in mvc profiler when we get there
+            http://ayende.com/blog/38913/ravendb-mvc-profiler-support
+
             AreaRegistration.RegisterAllAreas();
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
