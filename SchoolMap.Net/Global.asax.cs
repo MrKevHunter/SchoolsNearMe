ae091100b72bca83519b24100aeabf61ea4050a6
WestDiscGolf <git@icecubez.net>
2012-07-26 09:06:31 +0100

    Tidy up global.ascx.cs

--- SchoolMap.Net/Global.asax.cs
+++ SchoolMap.Net/Global.asax.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -10,21 +9,11 @@ using SchoolMap.Net.Models.Indexes;
 
 namespace SchoolMap.Net
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class WebApiApplication : HttpApplication
     {
         private const string RavenSessionKey = "RavenMVC.Session";
 
         public static IDocumentStore Store;
-
-
-
-        public WebApiApplication()
-        {
-            
-     }
 
         public static IDocumentSession CurrentSession
         {
@@ -33,9 +22,6 @@ namespace SchoolMap.Net
 
         protected void Application_Start()
         {
-            // to plumb in mvc profiler when we get there
-            //http: //ayende.com/blog/38913/ravendb-mvc-profiler-support
-
             Store = new EmbeddableDocumentStore { ConnectionStringName = "RavenDB" };
             Store.Initialize();
             IndexCreation.CreateIndexes(typeof(FindSchoolByName).Assembly, Store);
