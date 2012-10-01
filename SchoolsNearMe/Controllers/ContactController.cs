ce7761fbbb4c4a3c94edcbe788afa1cb63543c7b
Kev Hunter <MrKevHunter@gmail.com>
2012-10-01 20:30:09 +0100

    added a contact us form, now just need to wire it into some sort of email

--- SchoolsNearMe/Controllers/ContactController.cs
+++ SchoolsNearMe/Controllers/ContactController.cs
@@ -1,10 +1,34 @@
 ﻿using System.Web.Mvc;
+using SchoolsNearMe.Models;
 
 namespace SchoolsNearMe.Controllers
 {
     public class ContactController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactUs contactUs)
+        {
+            if (!ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(contactUs.From))
+                {
+                    ModelState.AddModelError("From","You must enter an email address");
+                }
+                if (string.IsNullOrEmpty(contactUs.Body))
+                {
+                    ModelState.AddModelError("Body", "You must enter a message");
+                }
+                return View();
+            }
+            return RedirectToAction("ContactConfirm");
+        }
+
+        public ActionResult ContactConfirm()
         {
             return View();
         }
---
+++ SchoolsNearMe/Models/ContactUs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolsNearMe.Models
+{
+    public class ContactUs
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string From { get; set; }
+        [Required]
+        public string Body { get; set; } 
+    }
+}
