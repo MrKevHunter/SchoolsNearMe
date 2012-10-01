82265b93e38e2694453da7cb77e6749b53d11544
Kev Hunter <MrKevHunter@gmail.com>
2012-10-01 21:32:22 +0100

    added glimpse and contact us form

--- SchoolsNearMe/Controllers/ContactController.cs
+++ SchoolsNearMe/Controllers/ContactController.cs
@@ -1,36 +1,20 @@
-﻿using System.Web.Mvc;
-using SchoolsNearMe.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
 
 namespace SchoolsNearMe.Controllers
 {
     public class ContactController : Controller
     {
+        //
+        // GET: /Contact/
+
         public ActionResult Index()
         {
             return View();
         }
 
-/*        [HttpPost]
-        public ActionResult Index(ContactUs contactUs)
-        {
-            if (!ModelState.IsValid)
-            {
-                if (string.IsNullOrEmpty(contactUs.From))
-                {
-                    ModelState.AddModelError("From","You must enter an email address");
-                }
-                if (string.IsNullOrEmpty(contactUs.Body))
-                {
-                    ModelState.AddModelError("Body", "You must enter a message");
-                }
-                return View();
-            }
-            return RedirectToAction("ContactConfirm");
-        }*/
-
-        public ActionResult ContactConfirm()
-        {
-            return View();
-        }
     }
 }
